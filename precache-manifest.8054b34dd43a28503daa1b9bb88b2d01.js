b79828b1ca656cdad8128c013f7c29c660945c87
Johnson Kit <johnsonkit@users.noreply.github.com>
2020-02-16 15:18:25 +0000

    Updates

---
+++ precache-manifest.8054b34dd43a28503daa1b9bb88b2d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e02d94371ac2ec8d5bf061246bbc1e",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "9c60116d2870ef098957",
+    "url": "/tic-tac-toe-react/static/css/main.feb5c604.chunk.css"
+  },
+  {
+    "revision": "c17b4a0cc14c346dbda1",
+    "url": "/tic-tac-toe-react/static/js/2.e9eaaf92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe-react/static/js/2.e9eaaf92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c60116d2870ef098957",
+    "url": "/tic-tac-toe-react/static/js/main.363db350.chunk.js"
+  },
+  {
+    "revision": "9e177b60dd6d1f2da0e9",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.939dfebc.js"
+  }
+]);
